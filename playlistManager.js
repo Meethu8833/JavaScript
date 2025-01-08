00f483f08d92fae2aeb9c52c2afd0d2c41296225
Meethu8833 <meethuprasanthkk@gmail.com>
2025-01-08 10:06:45 +0530

    08/06/25

---
+++ playlistManager.js
@@ -0,0 +1,36 @@
+const songs=["aaa","bbb","ccc","ddd","eee"]
+
+// songs.push("fff")
+// console.log(songs);
+
+
+// songs.pop()
+// console.log(songs);
+
+
+// console.log(songs.length)
+
+// l=songs.length-1
+// console.log("first element: ",songs.at(0));
+// console.log("last element: ",songs.at(l));
+
+// console.log(songs.join(","));
+
+
+songs2=["abc","bcd","cde"]
+console.log(songs.concat(songs2))
+
+
+// songs3=[["aaa","bbb"],["abc","cde"]]
+// console.log(songs3.flat())
+
+// songs.splice(0,2,"qwe","rty")
+// console.log(songs);
+
+
+// console.log(songs.sort());
+
+
+le=songs.length-2
+songs.copyWithin(le,0,2)
+console.log(songs);
